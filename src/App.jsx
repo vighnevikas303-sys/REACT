60d379fbfc3fcb05201a9930a64449c63c340438
vikas vighne <vighnevikas303@gmail.com>
2026-06-01 02:05:11 +0530

    bootsrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import XButton from "./xbutton";
 import Hello from "./Hello";
+import Randomf from "./Random";
 
 function App() {
   return <div>
-    <h1>hello I am Vikas!</h1>
+    <h3>hello I am Vikas!</h3>
     <XButton />
     <Hello />
+    <Randomf />
+    <Randomf />
+    <Randomf />
+    <Randomf />
+    <Randomf />
+
+
   </div>
 
 }
---
+++ src/Random.jsx
@@ -0,0 +1,5 @@
+function Randomf() {
+  let number = Math.random() * 10;
+  return <h1 style={{color:"orange"}} >Random number is : {Math.round(number)}</h1>
+}
+export default Randomf;
